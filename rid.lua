c71c0092be8cbbe6f541e58f3870374ea75e7dc4
xiaoyi-boop <1701291983@qq.com>
2025-05-18 12:57:50 +0800

    Add files via upload

---
+++ rid.lua
@@ -0,0 +1,5 @@
+if game.Players.LocalPlayer:IsInGroup(35938769) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/rjiaoben/refs/heads/main/r%E6%9C%AA%E5%8A%A0%E5%85%A5.lua",true))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/rjiaoben/refs/heads/main/Rjiaoben1.lua",true))()
+end
